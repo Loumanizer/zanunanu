7e3a4afff3ea4970c7459f8e933fd05e2bf95d82
Pranab Shrestha <molly.shrestha@gmail.com>
2017-11-08 00:46:59 -0500

    asf

--- TicTacToe/app/src/main/java/molly/shrestha/edu/oakland/tictactoe/Game.java
+++ TicTacToe/app/src/main/java/molly/shrestha/edu/oakland/tictactoe/Game.java
@@ -21,27 +21,24 @@ public class Game extends AppCompatActivity {
     public static int Game_CURRENT_STATUS = -1;
     public static final int Game_STATUS_ACEPTED = 3;
     public static final int Game_STATUS_DECLIED = 4;
-    public static final int Game_STATUS_MOVE_SEND = 9;
     public static final int Game_STATUS_MOVE_WAITING = 9;
     public static final int Game_STATUS_PlAYER1 = 5;
     public static final int Game_STATUS_PlAYER2 = 8;
     public static final int Game_STATUS_RECEIVED = 2;
     public static final int Game_STATUS_REQUEST_WAITING = 7;
-    public static final int Game_STATUS_SEND = 1;
     public static final int Game_STATUS_TERMINATED = 6;
     public static final int Player1 = 0;
     public static final int Player2 = 1;
-    public static int Player_Turn = -1;
     GridLayout buttonContainer = null;
     public int currentPlayer = Player1;
     public TTTGame game = null;
     SMSGameReceiver gamereceiver;
-    int[] playerinput = new int[this.totalbox];
+    public int totalbox = 9;
+    int[] playerinput = new int[totalbox];
     public Convert tablelocConvert = new Convert();
     TextView textPlayer1Time = null;
     TextView textPlayer2Time = null;
-    public TimeTracker timeTracker = new TimeTracker();
-    public int totalbox = 9;
+
     TextView tv = null;
     TextView tvCurrentPlayer = null;
     TextView tvGameStatus = null;
@@ -56,23 +53,20 @@ public class Game extends AppCompatActivity {
             int i = Game.Player1;
             TTTButton b = (TTTButton) v;
             b.setTag(Integer.valueOf(Game.this.currentPlayer));
-            Game.this.enablecells(false);
-            Game.this.game.getPlayer(Game.this.currentPlayer).MarkCell(b.index);
-            Game.this.tablelocConvert.getXYvalue(b.index);
+            //Game.this.enablecells(false);
+            game.getPlayer(Game.this.currentPlayer).MarkCell(b.index);
+            tablelocConvert.getXYvalue(b.index);
             String msg = "0978 : Tic-Tac-Toe : SELECTED : " + Game.this.tablelocConvert.toSting();
-            Game.Game_CURRENT_STATUS = Game.Game_STATUS_MOVE_WAITING;
-            Game.this.timeTracker.setStopTime(Game.this.currentPlayer);
-            Game.this.textPlayer1Time.setText(Game.this.game.getPlayer(Game.this.currentPlayer).name + " : Total Time : " + Game.this.timeTracker.getTotalTime(Game.this.currentPlayer));
-            Game.this.CheckWiner();
-            Game.this.tvGameStatus.setText("Move Send");
+            Game_CURRENT_STATUS = Game.Game_STATUS_MOVE_WAITING;
+            //CheckWiner();
+            tvGameStatus.setText("Move Send");
             Game game = Game.this;
-            if (Game.this.currentPlayer == 0) {
-                i = Game.Player2;
+            if (currentPlayer == 0) {
+                i = Player2;
             }
-            game.currentPlayer = i;
-            Game.this.tvCurrentPlayer.setText(Game.this.game.getPlayer(Game.this.currentPlayer).name);
-            SmsManager.getDefault().sendTextMessage(Game.this.game.getPlayer(Game.this.currentPlayer).phonenumber, null, msg, null, null);
-            Game.this.timeTracker.setStartTime();
+            currentPlayer = i;
+            tvCurrentPlayer.setText(Game.this.game.getPlayer(currentPlayer).name);
+            //SmsManager.getDefault().sendTextMessage(Game.this.game.getPlayer(currentPlayer).phonenumber, null, msg, null, null);
         }
     }
 
@@ -81,8 +75,6 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         this.tv = (TextView) findViewById(R.id.textView);
-        this.textPlayer1Time = (TextView) findViewById(R.id.textPlayer1Time);
-        //this.textPlayer2Time = (TextView) findViewById(R.id.textPlayer2Time);
         this.tvCurrentPlayer = (TextView) findViewById(R.id.tvCurrentPlayer);
         this.tvGameStatus = (TextView) findViewById(R.id.tvGamestatus);
         this.buttonContainer = (GridLayout) findViewById(R.id.buttonContainer);
@@ -94,7 +86,7 @@ public class Game extends AppCompatActivity {
         registerReceiver(this.gamereceiver, SMSReceiver.filter);
         this.game.getPlayer(Player2).symbol = 17301558;
         this.gamereceiver.setFlag(Player2);
-        for (int i = Player1; i < this.totalbox; i += Player2) {
+        for (int i = 0; i < totalbox; i += 1) {
             TTTButton b = new TTTButton(this);
             b.setIndex(i);
             b.setTag(Integer.valueOf(-1));
@@ -104,7 +96,7 @@ public class Game extends AppCompatActivity {
             this.buttonContainer.addView(b);
             this.buttons[i] = b;
         }
-        this.buttonContainer.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+        buttonContainer.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
             public void onGlobalLayout() {
                 int bContainerWidth = Game.this.buttonContainer.getWidth();
                 int bContainerHeight = Game.this.buttonContainer.getHeight();
@@ -123,17 +115,13 @@ public class Game extends AppCompatActivity {
         });
         if (this.game.getPlayer(Player1).firstplayer == Player2) {
             this.currentPlayer = Player1;
-            enablecells(true);
-            this.textPlayer1Time.setText(this.game.getPlayer(Player1).name + " : Total Time :");
-            this.textPlayer2Time.setText(this.game.getPlayer(Player2).name + " : Total Time :");
+            //enablecells(true);
+
         } else {
             this.currentPlayer = Player2;
-            enablecells(false);
-            this.textPlayer1Time.setText(this.game.getPlayer(Player1).name + " : Total Time :");
-            this.textPlayer2Time.setText(this.game.getPlayer(Player2).name + " : Total Time :");
+            //enablecells(false);
         }
         this.tvCurrentPlayer.setText(this.game.getPlayer(this.currentPlayer).name);
-        this.timeTracker.setStartTime();
     }
 
     public void UpdateGameTable(String phonenumber, String msgString) {
@@ -149,10 +137,10 @@ public class Game extends AppCompatActivity {
                     Intent intent = new Intent(Game.this.getBaseContext(), MainScreen.class);
                     Game.this.gamereceiver.setFlag(Game.Player1);
                     Game.this.startActivity(intent);
-                    intent.setFlags(131072);
-                    Game.this.startActivity(intent);
-                    Game.this.finish();
-                    dialog.dismiss();
+                    //intent.setFlags(131072);
+                    //Game.this.startActivity(intent);
+                    //Game.this.finish();
+                    //dialog.dismiss();
                 }
             });
             adDraw.show();
@@ -195,28 +183,36 @@ public class Game extends AppCompatActivity {
         this.buttons[xlocation].setEnabled(false);
         this.buttons[xlocation].setTag(Integer.valueOf(this.currentPlayer));
         this.game.getPlayer(this.currentPlayer).MarkCell(this.buttons[xlocation].index);
-        enablecells(true);
+        //enablecells(true);
         this.tvGameStatus.setText("Move Recerived");
-        this.timeTracker.setStopTime(this.currentPlayer);
-        this.textPlayer2Time.setText(this.game.getPlayer(this.currentPlayer).name + " : Total Time : " + this.timeTracker.getTotalTime(this.currentPlayer));
         CheckWiner();
         if (this.currentPlayer != 0) {
             i = Player1;
         }
         this.currentPlayer = i;
         this.tvCurrentPlayer.setText(this.game.getPlayer(this.currentPlayer).name);
-        this.timeTracker.setStartTime();
     }
 
     public void CheckWiner() {
         int i;
         String Playername = this.game.getPlayer(this.currentPlayer).name;
-        int cp = this.currentPlayer;
-        int playercur = this.currentPlayer;
-        for (i = Player1; i < this.totalbox; i += Player2) {
-            this.playerinput[i] = ((Integer) this.buttons[i].getTag()).intValue();
+        int cp = currentPlayer;
+        for (i = 0; i < totalbox; i += 1) {
+
+            //playerinput[i] = ((Integer) this.buttons[i].getTag()).intValue();
+           // playerinput[i] = ((Integer)buttons[i].getTag()).intValue();
         }
-        if ((this.playerinput[Player1] == cp && this.playerinput[Player2] == cp && this.playerinput[Game_STATUS_RECEIVED] == cp && this.playerinput[Game_STATUS_ACEPTED] == cp) || ((this.playerinput[Game_STATUS_DECLIED] == cp && this.playerinput[Game_STATUS_PlAYER1] == cp && this.playerinput[Game_STATUS_TERMINATED] == cp && this.playerinput[Game_STATUS_REQUEST_WAITING] == cp) || ((this.playerinput[Game_STATUS_PlAYER2] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[10] == cp && this.playerinput[11] == cp) || ((this.playerinput[12] == cp && this.playerinput[13] == cp && this.playerinput[14] == cp && this.playerinput[15] == cp) || ((this.playerinput[Player1] == cp && this.playerinput[Game_STATUS_DECLIED] == cp && this.playerinput[Game_STATUS_PlAYER2] == cp && this.playerinput[12] == cp) || ((this.playerinput[Player2] == cp && this.playerinput[Game_STATUS_PlAYER1] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[13] == cp) || ((this.playerinput[Game_STATUS_RECEIVED] == cp && this.playerinput[Game_STATUS_TERMINATED] == cp && this.playerinput[10] == cp && this.playerinput[14] == cp) || ((this.playerinput[Game_STATUS_ACEPTED] == cp && this.playerinput[Game_STATUS_REQUEST_WAITING] == cp && this.playerinput[11] == cp && this.playerinput[15] == cp) || ((this.playerinput[Player1] == cp && this.playerinput[Game_STATUS_PlAYER1] == cp && this.playerinput[10] == cp && this.playerinput[15] == cp) || (this.playerinput[Game_STATUS_ACEPTED] == cp && this.playerinput[Game_STATUS_TERMINATED] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[12] == cp)))))))))) {
+
+
+
+        if ((this.playerinput[Player1] == cp &&
+                this.playerinput[Player2] == cp &&
+                this.playerinput[Game_STATUS_RECEIVED] == cp &&
+                this.playerinput[Game_STATUS_ACEPTED] == cp) ||
+                ((this.playerinput[Game_STATUS_DECLIED] == cp &&
+                this.playerinput[Game_STATUS_PlAYER1] == cp &&
+                 this.playerinput[Game_STATUS_TERMINATED] == cp &&
+                  this.playerinput[Game_STATUS_REQUEST_WAITING] == cp) || ((this.playerinput[Game_STATUS_PlAYER2] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[10] == cp && this.playerinput[11] == cp) || ((this.playerinput[12] == cp && this.playerinput[13] == cp && this.playerinput[14] == cp && this.playerinput[15] == cp) || ((this.playerinput[Player1] == cp && this.playerinput[Game_STATUS_DECLIED] == cp && this.playerinput[Game_STATUS_PlAYER2] == cp && this.playerinput[12] == cp) || ((this.playerinput[Player2] == cp && this.playerinput[Game_STATUS_PlAYER1] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[13] == cp) || ((this.playerinput[Game_STATUS_RECEIVED] == cp && this.playerinput[Game_STATUS_TERMINATED] == cp && this.playerinput[10] == cp && this.playerinput[14] == cp) || ((this.playerinput[Game_STATUS_ACEPTED] == cp && this.playerinput[Game_STATUS_REQUEST_WAITING] == cp && this.playerinput[11] == cp && this.playerinput[15] == cp) || ((this.playerinput[Player1] == cp && this.playerinput[Game_STATUS_PlAYER1] == cp && this.playerinput[10] == cp && this.playerinput[15] == cp) || (this.playerinput[Game_STATUS_ACEPTED] == cp && this.playerinput[Game_STATUS_TERMINATED] == cp && this.playerinput[Game_STATUS_MOVE_WAITING] == cp && this.playerinput[12] == cp)))))))))) {
             Playername = this.game.getPlayer(this.currentPlayer).name;
             this.tvGameStatus.setText(Playername + " : winner");
             AlertDialog ad = new Builder(this).create();
@@ -228,10 +224,10 @@ public class Game extends AppCompatActivity {
                     Intent intent = new Intent(Game.this.getBaseContext(), MainScreen.class);
                     Game.this.gamereceiver.setFlag(Game.Player1);
                     Game.this.startActivity(intent);
-                    intent.setFlags(131072);
-                    Game.this.startActivity(intent);
-                    Game.this.finish();
-                    dialog.dismiss();
+                    //intent.setFlags(131072);
+                    //Game.this.startActivity(intent);
+                    //Game.this.finish();
+                    //dialog.dismiss();
                 }
             });
             ad.show();
@@ -250,10 +246,10 @@ public class Game extends AppCompatActivity {
                     Intent intent = new Intent(Game.this.getBaseContext(), MainScreen.class);
                     Game.this.gamereceiver.setFlag(Game.Player1);
                     Game.this.startActivity(intent);
-                    intent.setFlags(131072);
-                    Game.this.startActivity(intent);
-                    Game.this.finish();
-                    dialog.dismiss();
+                    //intent.setFlags(131072);
+                    //Game.this.startActivity(intent);
+                    //Game.this.finish();
+                    //dialog.dismiss();
                 }
             });
             adDraw.show();
